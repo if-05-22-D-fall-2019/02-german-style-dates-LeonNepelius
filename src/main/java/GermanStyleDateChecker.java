0dd8fc9903d6f312c34486d0569e46cd26334305
AdventureT <le@nepelius.at>
2020-05-25 16:32:49 +0200

    Optimised Regex

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,10 +3,10 @@ import java.util.regex.Pattern;
 
 class GermanStyleDateChecker {
     static Matcher getMatcher(String dateString) {
-        String dayRegex = "([0-2]?[1-9]|[3][0-1])";
+        String dayRegex = "([0-2]?[1-9]|3[0-1])";
         String separatorRegex = "(\\.|\\-|\\s)";
-        String monthRegex = "([0]?[1-9]|[1][0-2])";
-        String yearRegex = "([2]\\d{3}|[1][9]\\d{2}|\\d{2})";
+        String monthRegex = "(0?[1-9]|1[0-2])";
+        String yearRegex = "((19|20)?\\d{2})";
         String finalRegex = String.format("^%s%s%s%s%s$", dayRegex, separatorRegex, monthRegex, separatorRegex, yearRegex);
         Pattern pattern = Pattern.compile(finalRegex);
         return pattern.matcher(dateString);
